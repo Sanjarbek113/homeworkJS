613fe464017e56c54f23a51ff59e9a6567134fe4
Sanjarbek113 <matyoqubovsanjarbek81@gmail.com>
2022-01-28 11:46:52 +0500

    add

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("SALOM");
-const main = document.querySelector(".main")
+const main = document.querySelector(".main");
+const main_flex = document.querySelector(".main-flex");
 const inputName = document.querySelector(".input-name");
 const inputTel = document.querySelector(".input-tel");
 const inputAge = document.querySelector(".input-age");
@@ -8,27 +9,52 @@ const btn = document.querySelector(".btn");
 const pElements = document.querySelectorAll("p") ;
 const button = document.querySelector(".img");
 
+
 // console.log(pElements[0]);
 
 btn.addEventListener("click", e =>{
-    let name = inputName.value;    
+    const olElement = document.createElement("ol");
+    const liElemet1= document.createElement('li');
+    const liElemet2= document.createElement('li');
+    const liElemet3= document.createElement('li');
+    const liElemet4= document.createElement('li');
+    const deletebtn = document.createElement("button");
+    deletebtn.textContent = "delete";
+    main_flex.appendChild(olElement);
+    olElement.classList.add("box-list");
+    olElement.appendChild(liElemet1);
+    olElement.appendChild(liElemet2);
+    olElement.appendChild(liElemet3);
+    olElement.appendChild(liElemet4);
+    olElement.appendChild(deletebtn);
+    deletebtn.classList.add("delete-btn");
+    olElement.classList.add("list");
+    liElemet1.textContent = inputName.value;    
     inputName.value = "";
-    pElements[0].textContent = "Ismi :" + name;
-    let tel = inputTel.value;
+    liElemet2.textContent = inputTel.value;
     inputTel.value = "";
-    pElements[1].textContent = "Tel raqami: " + tel;
-    let age = inputAge.value;
+    liElemet3.textContent = inputAge.value;
     inputAge.value = "";
-    pElements[2].textContent = "Age :" + age;
-    let text = inputText.value;
+    liElemet4.textContent = inputText.value;
     inputText.value = "";
-    pElements[3].textContent = "Interests " + text;
 
-
+  
+deletebtn.addEventListener("click", e =>{
+    liElemet1.textContent ="";
+    liElemet2.textContent ="";
+    liElemet3.textContent ="";
+    liElemet4.textContent ="";
+    deletebtn.textContent ="";
+    deletebtn.classList.remove("delete-btn");
+    olElement.classList.remove("box-list");
+    olElement.classList.remove("list");
+})
 })
 
-button.addEventListener("click", e =>{
-    for(let key of pElements){
-        key.textContent =" Kiritilmagan";
-    }
+deletebtn.addEventListener("click", e =>{
+    liElemet1.textContent ="";
+    liElemet2.textContent ="";
+    liElemet3.textContent ="";
+    liElemet4.textContent ="";
+   
 })
